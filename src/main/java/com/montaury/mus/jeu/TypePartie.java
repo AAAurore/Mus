c5bd46f5f82b16a29216bcbb18a60de2400ef4ea
Levelu Pierre <plevelu@iutbayonne.univ-pau.fr>
2021-02-25 16:49:42 +0100

    Ajout de l'énumération TypePartie pour gérer les version solo ou multi

---
+++ src/main/java/com/montaury/mus/jeu/TypePartie.java
@@ -0,0 +1,13 @@
+package com.montaury.mus.jeu;
+
+    public enum TypePartie {
+        MULTIORDIS("MultiOrdis"),
+        SOLOORDI("SoloOrdi");
+
+        private final String nom;
+
+        TypePartie(String nom) {
+            this.nom = nom;
+        }
+    }
+
